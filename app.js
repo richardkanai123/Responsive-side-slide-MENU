24a807a287f256a8c3a78b2b82ad8d9b3f6ff056
richardkanai123 <richardkanainjeri@gmail.com>
2021-04-26 09:04:45 +0300

    css html done, js remaining

---
+++ app.js
@@ -0,0 +1,13 @@
+// get elements
+const togglebtn = document.querySelector('.toggle');
+const maincontainer = document.querySelector('.maincontainer');
+const menu = document.querySelector('.menuHolder');
+
+
+togglebtn.addEventListener('click', showMenu);
+
+
+function showMenu(){
+    // prompt('ready?',"yes","no");
+
+}
