42a8063eefae2e04dc481258cd764b7cc5a084cb
richardkanai123 <richardkanainjeri@gmail.com>
2021-04-27 14:17:55 +0300

    The SLIDE STUFF WORKS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const togglebtn = document.querySelector('.toggle');
 const maincontainer = document.querySelector('.maincontainer');
 const menu = document.querySelector('.menuHolder');
+const h1 = document.querySelector('h1');
+const nav = document.querySelector('nav');
 
 
 togglebtn.addEventListener('click', showMenu);
@@ -9,5 +11,33 @@ togglebtn.addEventListener('click', showMenu);
 
 function showMenu(){
     // prompt('ready?',"yes","no");
-
+    togglebtn.classList.toggle('Close');
+    maincontainer.classList.toggle('Close');
+    menu.classList.toggle('Close');
 }
+
+
+const Shop = nav.children[1];
+const About = nav.children[2]; 
+const Contact= nav.children[3];
+// change content per menu
+Shop.addEventListener('click',shop);
+
+function shop(){
+    h1.textContent="Shop";
+    maincontainer.textContent=" shop^30.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum minima enim deserunt quia, nam officiis quaerat veniam, ipsum distinctio soluta alias magni ad inventore! Pariatur doloribus aperiam maxime aspernatur fugiat impedit nulla expedita sit iste saepe. Error animi earum veritatis vitae corrupti vero! Quas quidem accusantium pariatur saepe in! SHOP";
+};
+
+About.addEventListener('click',about);
+
+function about(){
+    h1.textContent="About";
+    maincontainer.textContent=" About.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum minima enim deserunt quia, nam officiis quaerat veniam, ipsum distinctio soluta alias magni ad inventore! Pariatur doloribus aperiam maxime aspernatur fugiat impedit nulla expedita sit iste saepe. Error animi earum veritatis vitae corrupti vero! Quas quidem accusantium pariatur saepe in! SHOP";
+};
+
+Contact.addEventListener('click', ontact);
+
+function ontact(){
+    h1.textContenContact="Contact us";
+    maincontainer.textContent=" Contact Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum minima enim deserunt quia, nam officiis quaerat veniam, ipsum distinctio soluta alias magni ad inventore! Pariatur doloribus aperiam maxime aspernatur fugiat impedit nulla expedita sit iste saepe. Error animi earum veritatis vitae corrupti vero! Quas quidem accusantium pariatur saepe in! SHOP";
+};
